be66df9ea1e06ee9c74aaecbce6161f0a25881a1
Muhammad Wasay <gz2750114@gmail.com>
2025-08-17 02:01:03 -0700

    Fix scrollbar issue - change Section to min-h-screen

--- src/app/components/Section.jsx
+++ src/app/components/Section.jsx
@@ -1,6 +1,6 @@
 export default function Section({ children, className }) {
   return (
-    <section className={`h-screen w-full ${className || ""}`}>
+    <section className={`min-h-screen w-full ${className || ""}`}>
       {children}
     </section>
   );
